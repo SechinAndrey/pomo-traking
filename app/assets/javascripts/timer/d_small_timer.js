4e7a0d0778e516b4598bf6d2cc4f8fe27e8b72e8
Sechin Andrey <sechinandreyv@gmail.com>
2017-01-24 15:23:10 +0200

    add info panel

--- app/assets/javascripts/timer/d_small_timer.js
+++ app/assets/javascripts/timer/d_small_timer.js
@@ -2,6 +2,7 @@ angular.module('pomoTracking')
     .directive("smallTimer", function() {
         return {
             restrict: 'E',
+            replace: true,
             templateUrl: "timer/_small_timer.html",
             controller: 'TimerCtrl'
         };
