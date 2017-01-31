3e0a1dd5076500ce783510eea073d4a39cbf004d
Sechin Andrey <sechinandreyv@gmail.com>
2017-01-31 11:09:01 +0200

    d_big_timer refactor

--- app/assets/javascripts/timer/d_big_timer.js
+++ app/assets/javascripts/timer/d_big_timer.js
@@ -13,30 +13,25 @@ angular.module('pomoTracking')
                 var timer = document.getElementById("timer");
                 var context = timer.getContext('2d');
                 var context2 = document.getElementById("timer0").getContext('2d');
-                var rad = 80; // Радиус круга
-
+                var rad = 90; // Радиус круга
 
                 context.lineWidth = 12; // Ширина обводки
-
 
                 context2.lineWidth = 1;
                 context2.strokeStyle = "grey";
                 context2.arc(timer.width/2, timer.height/2, rad, 0, 2 * Math.PI, true);
                 context2.stroke();
 
+                var renderTimer = function(){
+                    var p = 2 * $scope.pomodoro.time/(1500*1000);
+                    context.clearRect(0,0,timer.width,timer.height);
+                    context.beginPath();
+                    context.arc(timer.width/2, timer.height/2, rad, 0, p * Math.PI, false);
+                    context.strokeStyle = 'grey';
+                    context.stroke();
+                };
 
-                $scope.$watch(
-                    'pomodoro.sec',
-                    function(){
-                        var p = 2 * $scope.pomodoro.time/(1500*1000);
-                        context.clearRect(0,0,timer.width,timer.height);
-                        context.beginPath();
-                        context.arc(timer.width/2, timer.height/2, rad, 0, p * Math.PI, false);
-                        context.strokeStyle = 'grey';
-                        context.stroke();
-                    });
-
-
+                $scope.$watch( 'pomodoro.sec', renderTimer);
             }
         };
     }]);
