d806101697fcf7802883664f9ab355cede4dd79f
Sechin Andrey <sechinandreyv@gmail.com>
2017-02-14 13:11:36 +0200

    add figaro

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,3 @@
 require "redis"
 
-REDIS = Redis.new(url: "redis://h:p7dcfa52979100ec4d8f13639c5d66bda1913ac1a885449b22a2efcddaa124004@ec2-79-125-18-120.eu-west-1.compute.amazonaws.com:13329")
+REDIS = Redis.new(url:  ENV["REDIS_URL"])
