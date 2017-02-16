0d9378dd984381b668fa6c60405f5a1b18c0a580
Sechin Andrey <sechinandreyv@gmail.com>
2017-02-16 17:33:35 +0200

    user model validates

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,7 +3,7 @@ class Project < ApplicationRecord
   has_many :pomo_cycles, dependent: :destroy
 
   validates :title, :pomo_time, :short_break_time, :long_break_time, presence: true
-  validates :title, length: { maximum: 50 }
+  validates :title, length: { maximum: 100 }
   validates :pomo_time, :short_break_time, :long_break_time, numericality: {
       only_integer: true,
       greater_than_or_equal_to: 1,
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,21 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+
   has_one :avatar, :dependent => :destroy
   has_many :projects, :dependent => :destroy
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :email, :username, :pomo_time, :short_break_time, :long_break_time, presence: true
+  validates :email, length: { maximum: 320 }
+  validates :username, length: { maximum: 100 }
+  validates :pomo_time, :short_break_time, :long_break_time, numericality: {
+      only_integer: true,
+      greater_than_or_equal_to: 1,
+      less_than_or_equal_to: 100
+  }
 
   def pomo_started?
     self.current_project_status == 'started'
