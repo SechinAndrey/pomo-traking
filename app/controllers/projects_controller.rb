9f1e09f507c8598992832eb68b16a79f646b6f76
Sechin Andrey <sechinandreyv@gmail.com>
2017-02-18 13:13:55 +0200

    -c

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,6 @@
 class ProjectsController < ApplicationController
   before_action :authenticate_user!
 
-
   def index
     render json: current_user.projects.all.as_json(only: [:id, :title])
   end
