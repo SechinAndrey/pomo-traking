0848fb46a53c12c5a5deae63985c1a8e4cdfab2b
Sechin Andrey <sechinandreyv@gmail.com>
2017-05-11 13:57:12 +0300

    one project statics

--- app/assets/javascripts/account/accountCtrl.js
+++ app/assets/javascripts/account/accountCtrl.js
@@ -3,17 +3,16 @@ angular.module('pomoTracking')
     '$scope',
     'Auth',
     function($scope, Auth){
-        var config = {
-            headers: {
-                'X-HTTP-Method-Override': 'PUT'
-            }
-        };
-
         Auth.currentUser().then(function (current_user) {
             $scope.user = current_user;
         });
 
         $scope.update = function () {
+            var config = {
+                headers: {
+                    'X-HTTP-Method-Override': 'PUT'
+                }
+            };
             Auth.register($scope.user, config).then(function(registeredUser) {
             }, function(error) {
             });
--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -1,21 +1,42 @@
 class ProjectSerializer < ActiveModel::Serializer
-  attributes :id, :title, :pomo_cycle, :pomo_count, :time,
+  attributes :id, :title, :pomo_cycle,
+             :pomo_count, :time, :pomo_cycle_count, :short_break_count, :long_break_count,
              :pomo_duration, :short_break_duration, :long_break_duration
 
   def pomo_cycle
     PomoCycle.where(id: object.pomo_cycle&.id).last.serialize
   end
 
+  ### statistics
   def pomo_count
     @pomo_count = object.statistics.count * 4
-    @pomo_count += object.pomo_cycle.periods.where(periods_type: 'pomo', ended: true).count if object.pomo_cycle
+    @periods = object.pomo_cycle&.periods&.where(periods_type: 'pomo', ended: true)
+    @pomo_count += @periods.count if object.pomo_cycle
     @pomo_count
   end
 
   def time
-    "#{@pomo_count/60}:#{@pomo_count%60}"
+    work_minutes = object.statistics.sum(:work_minutes)
+    @periods&.each do |period|
+      work_minutes += period.duration
+    end
+    time_str work_minutes
   end
 
+  def pomo_cycle_count
+    @pomo_cycle_count = object.statistics.count
+  end
+
+  def short_break_count
+    sb_count = object.statistics.count
+    sb_count += object.pomo_cycle&.periods&.where(periods_type: 'short break', ended: true).count
+  end
+
+  def long_break_count
+    @pomo_cycle_count
+  end
+
+  ### durations
   def pomo_duration
     object.duration_settings&.pomo_duration || object.user.duration_settings.pomo_duration
   end
@@ -27,4 +48,13 @@ class ProjectSerializer < ActiveModel::Serializer
   def long_break_duration
     object.duration_settings&.long_break_duration || object.user.duration_settings.long_break_duration
   end
+
+  private
+
+  def time_str work_minutes
+    hours = ('00'+(work_minutes/60).to_s.split('.')[0]).slice(-2, 2)
+    mins = ('00'+(work_minutes%60).to_s.split('.')[0]).slice(-2,2)
+    "#{hours}:#{mins}"
+  end
+
 end
